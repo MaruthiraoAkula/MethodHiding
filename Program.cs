68714128fefbf5c485576e6e2cdbb1712d7869d1
Maruthi Akula <maruthiraoakula22@gmail.com>
2022-07-06 11:25:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodHiding
+{
+    public class Baseclass
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("I'm a Base Class");
+        }
+    }
+    public class Derivedclass : Baseclass
+    {
+        public new void print()
+        {
+            //base.print();
+            Console.WriteLine("I'm a Derived Class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baseclass obj = new Derivedclass();
+            // Derivedclass obj = new Derivedclass();
+            obj.print();
+            //  ((Baseclass)obj).print();
+
+
+
+        }
+    }
+}
